c1810f23135d08d98a413f78e420f0a1c3c36550
tita-ubuntu-vm <i.nimah@tue.nl>
2021-08-31 16:26:10 +0100

    Updating bash files

--- eval/eval_oproto_intent.sh
+++ eval/eval_oproto_intent.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 source HOME_DIR_INSTALLATION/miniconda3/etc/profile.d/conda.sh
-conda activate CONDA_ENV
+conda activate protoinfomax_env
 cd ../src
 
 # Evaluate OProto on intent classification K=100 N=1
