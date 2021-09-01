f5555cc7d27c35d1a210bfc51173eb151047cf44
tita-ubuntu-vm <i.nimah@tue.nl>
2021-09-01 12:46:44 +0100

    Deleting files

--- eval/eval_protonet_sentiment.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-source HOME_DIR_INSTALLATION/miniconda3/etc/profile.d/conda.sh
-conda activate protoinfomax_env
-cd ../src
-
-# Evaluate Proto-net on sentiment classification K=100
-python eval_proto.py -config ../config/config_sentiment -section test-run > ../eval/eval_proto_sentiment.txt
